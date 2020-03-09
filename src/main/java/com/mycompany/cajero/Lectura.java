048a99e879210f6e6b00a29cc181416c1e6c2439
henrymeister <enriquemumanrique@gmail.com>
2020-03-09 13:25:15 +0100

    Añadidos comentarios a lectura

--- src/main/java/com/mycompany/cajero/Lectura.java
+++ src/main/java/com/mycompany/cajero/Lectura.java
@@ -1,26 +1,29 @@
-
 package com.mycompany.cajero;
 
 import java.util.Scanner;
 
 public class Lectura {
     public static String cadena(){
+        //Pido por pantalla el valor
         Scanner sc = new Scanner (System.in);
         String cadena=sc.nextLine();
+        //Devuelvo el valor pedido por pantalla
         return cadena;
     }
 
     public static int entero(){
         Scanner sc = new Scanner (System.in);
+        //Pido por pantalla el valor
         String string=sc.nextLine();
         int entero=0;
-        try {
+        try{
+            //Cambio el valor a un entero
             entero=Integer.parseInt(string);
-        } 
-        catch (NumberFormatException ex) 
-        {
+        }catch (NumberFormatException ex){
+            //Si no es un entero devuelvo error
             System.out.println(ex);
         }
+        //Devuelvo el valor pedido por pantalla
         return entero;
     }
     
